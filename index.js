b0e97fb78c81e662c4a0b1a7a1e365f85ac99abb
Nicole Sophie Belger <n.belger@hbk-bs.de>
2025-06-02 14:12:30 +0200

    bjbk

--- index.js
+++ index.js
@@ -1,16 +1,119 @@
-const response = await fetch('https://www.val.town/v/YOUR_USER_NAME/openai_api', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    messages: [
-      {
-        role: 'user',
-        content: 'Hello world'
-      }
-    ]
-  })
+//@ts-check
+// [x]. get the content from the input element
+// [x]. send the content to the val town endpoint using fetch POST request
+// [x]. await the response
+// [x]. get the json from the response
+// [x]. Add the user message to the .chat-history
+
+// How to control the behaviour of the chat bot?
+
+// Bonus:
+// What happens if the context gets to long?
+// What happens if the chat-history window get s to full (scolling)
+
+let messageHistory = {
+	// messages: [{role: user | assistant | system; content: string}]
+	response_format: { type: 'json_object' },
+	messages: [
+		{
+			role: 'system',
+			content: `
+			Your are an ascii art generator. This is some example.
+			---example
+			➬➻➮➯➽⟿⤐⥴↦⇏➬➱⤇△✩❈❇︎✷
+			---
+			Only generate ascii art. You are free to use more icons. response in JSON
+			`,
+		},
+	],
+};
+
+// TODO: use your own val.town endpoint
+// remix: https://val.town/remix/ff6347-openai-api
+const apiEndpoint = 'https://www.val.town/x/ff6347/openai_api';
+if (!apiEndpoint.includes('run')) {
+	throw new Error('Please use your own val.town endpoint!!!');
+}
+
+const MAX_HISTORY_LENGTH = 10;
+
+document.addEventListener('DOMContentLoaded', () => {
+	// get the history element
+	const chatHistoryElement = document.querySelector('.chat-history');
+	const inputElement = document.querySelector('input');
+	const formElement = document.querySelector('form');
+	// check if the elements exists in the DOM
+	if (!chatHistoryElement) {
+		throw new Error('Could not find element .chat-history');
+	}
+	if (!formElement) {
+		throw new Error('Form element does not exists');
+	}
+	if (!inputElement) {
+		throw new Error('Could not find input element');
+	}
+	// run a function when the user hits send
+	formElement.addEventListener('submit', async (event) => {
+		event.preventDefault(); // dont reload the page
+
+		const formData = new FormData(formElement);
+		const content = formData.get('content');
+		if (!content) {
+			throw new Error("Could not get 'content' from form");
+		}
+		//@ts-ignore
+		messageHistory.messages.push({ role: 'user', content: content });
+		messageHistory = truncateHistory(messageHistory);
+		chatHistoryElement.innerHTML = addToChatHistoryElement(messageHistory);
+		inputElement.value = '';
+		scrollToBottom(chatHistoryElement);
+
+		const response = await fetch(apiEndpoint, {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify(messageHistory),
+		});
+
+		if (!response.ok) {
+			const errorText = await response.text();
+			throw new Error(errorText);
+		}
+
+		const json = await response.json();
+		console.log(json);
+		// @ts-ignore
+		messageHistory.messages.push(json.completion.choices[0].message);
+		messageHistory = truncateHistory(messageHistory);
+
+		chatHistoryElement.innerHTML = addToChatHistoryElement(messageHistory);
+		scrollToBottom(chatHistoryElement);
+	});
 });
 
-const data = await response.json();
+function addToChatHistoryElement(mhistory) {
+	const htmlStrings = mhistory.messages.map((message) => {
+		return message.role === 'system'
+			? ''
+			: `<div class="message ${message.role}">${message.content}</div>`;
+	});
+	return htmlStrings.join('');
+}
+
+function scrollToBottom(conainer) {
+	conainer.scrollTop = conainer.scrollHeight;
+}
+
+function truncateHistory(h) {
+	if (!h || !h.messages || h.messages.length <= 1) {
+		return h; // No truncation needed or possible
+	}
+	const { messages } = h;
+	const [system, ...rest] = messages;
+	if (rest.length - 1 > MAX_HISTORY_LENGTH) {
+		return { messages: [system, ...rest.slice(-MAX_HISTORY_LENGTH)] };
+	} else {
+		return h;
+	}
+}
